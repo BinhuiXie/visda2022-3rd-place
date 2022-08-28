9a8c12b2d66213c2a920204e2da7f280e55cf7d9
binhuixie <binhuixie@bit.edu.cn>
2022-08-28 14:09:41 +0800

    update

---
+++ scripts/2_segformer_synaug.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=2 python -m tools.train configs/source_only/synaug_to_zerov2_segformer.json --work-dir experiment/baseline/synaug_to_zerov2_segformer/
+CUDA_VISIBLE_DEVICES=2 python -m tools.test configs/source_only/synaug_to_zerov2_segformer.json experiment/baseline/synaug_to_zerov2_segformer/latest.pth --format-only --show-dir experiment/baseline/synaug_to_zerov2_segformer/predictions --opacity 1
+CUDA_VISIBLE_DEVICES=2 python -m tools.convert_visuals_to_labels experiment/baseline/synaug_to_zerov2_segformer/predictions experiment/baseline/synaug_to_zerov2_segformer/original/
