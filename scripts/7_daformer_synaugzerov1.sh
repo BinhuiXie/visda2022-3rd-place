3ad958526137a1071969a22a0e2c0b1fa5fb647f
binhuixie <binhuixie@bit.edu.cn>
2022-08-29 09:51:10 +0800

    synaugv1 again

--- scripts/7_daformer_synaugzerov1.sh
+++ scripts/7_daformer_synaugzerov1.sh
@@ -4,6 +4,6 @@
 
 
 
-CUDA_VISIBLE_DEVICES=7 python -m tools.train configs/daformer/norcs/synaugzerov1_to_zerov2_daformer_mit5.py --work-dir experiment/baseline/norcs/Synaugzerov1_to_zerov2_daformer_mit5/
-CUDA_VISIBLE_DEVICES=7 python -m tools.test configs/daformer/norcs/synaugzerov1_to_zerov2_daformer_mit5.py experiment/baseline/norcs/Synaugzerov1_to_zerov2_daformer_mit5/latest.pth --format-only --show-dir experiment/baseline/norcs/Synaugzerov1_to_zerov2_daformer_mit5/predictions --opacity 1
+CUDA_VISIBLE_DEVICES=7 python -m tools.train configs/daformer/norcs/Synaugzerov1_to_zerov2_daformer_mit5.py --work-dir experiment/baseline/norcs/Synaugzerov1_to_zerov2_daformer_mit5/
+CUDA_VISIBLE_DEVICES=7 python -m tools.test configs/daformer/norcs/Synaugzerov1_to_zerov2_daformer_mit5.py experiment/baseline/norcs/Synaugzerov1_to_zerov2_daformer_mit5/latest.pth --format-only --show-dir experiment/baseline/norcs/Synaugzerov1_to_zerov2_daformer_mit5/predictions --opacity 1
 CUDA_VISIBLE_DEVICES=7 python -m tools.convert_visuals_to_labels experiment/baseline/norcs/Synaugzerov1_to_zerov2_daformer_mit5/predictions experiment/baseline/norcs/Synaugzerov1_to_zerov2_daformer_mit5/original/
