fa467da97265e918967e5c8cdda0325c28267d42
binhuixie <binhuixie@bit.edu.cn>
2022-09-02 14:29:57 +0800

    crop size

---
+++ scripts/crop_size/2_zerov1all_daformer_SegF_640.sh
@@ -0,0 +1,12 @@
+## source domain: zerowaste-f/all
+## target domain: zerowaste-v2-splits/train
+# SegFormer - source only
+CUDA_VISIBLE_DEVICES=2 python -m tools.train configs/source_only/640x640/zerov1all_to_zerov2_segformer.json --work-dir experiment/baseline/5_class_640x640/zerov1all_to_zerov2_segformer/
+CUDA_VISIBLE_DEVICES=2 python -m tools.test configs/source_only/640x640/zerov1all_to_zerov2_segformer.json experiment/baseline/5_class_640x640/zerov1all_to_zerov2_segformer/latest.pth --format-only --show-dir experiment/baseline/5_class_640x640/zerov1all_to_zerov2_segformer/predictions --opacity 1
+CUDA_VISIBLE_DEVICES=2 python -m tools.convert_visuals_to_labels experiment/baseline/5_class_640x640/zerov1all_to_zerov2_segformer/predictions experiment/baseline/5_class_640x640/zerov1all_to_zerov2_segformer/original/
+
+## DAFormer - uda
+CUDA_VISIBLE_DEVICES=2 python -m tools.train configs/daformer/640x640/zerov1all_to_zerov2_daformer_mit5.py --work-dir experiment/baseline/5_class_640x640/zerov1all_to_zerov2_daformer_mit5/
+CUDA_VISIBLE_DEVICES=2 python -m tools.test configs/daformer/640x640/zerov1all_to_zerov2_daformer_mit5.py experiment/baseline/5_class_640x640/zerov1all_to_zerov2_daformer_mit5/latest.pth --format-only --show-dir experiment/baseline/5_class_640x640/zerov1all_to_zerov2_daformer_mit5/predictions --opacity 1
+CUDA_VISIBLE_DEVICES=2 python -m tools.convert_visuals_to_labels experiment/baseline/5_class_640x640/zerov1all_to_zerov2_daformer_mit5/predictions experiment/baseline/5_class_640x640/zerov1all_to_zerov2_daformer_mit5/original/
+
