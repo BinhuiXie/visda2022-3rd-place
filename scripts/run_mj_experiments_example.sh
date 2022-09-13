7db4b68b2354cc395378bec2ddf7cccd4d263307
KiwiXR <kiwi2364261522@outlook.com>
2022-09-13 14:50:27 +0800

    workflow for run_mj_experiments.py

---
+++ scripts/run_mj_experiments_example.sh
@@ -0,0 +1,39 @@
+# STEP 1: Training
+
+# To run an experiment, first set the parameters in `mj_experiments.py` and take the exp id here
+# e.g., for exp 1
+CUDA_VISIBLE_DEVICES=0 python run_mj_experiments.py --exp 1
+# The working directory is set to `./work_dirs_mj`. Change it in run_mj_experiments.py if you would like to.
+# The previous logs are saved to `mjlogs`. My preference for this step is:
+## CUDA_VISIBLE_DEVICES=0 nohup python run_mj_experiments.py --exp 1 > mjlogs/exp1.log >&1 &
+
+# STEP 2: Evaluate and do palette
+
+# This step evaluates the model and outputs corresponding test set labels (zero v2 val set for now)
+# Gets visuals here
+CUDA_VISIBLE_DEVICES=0 python tools.test \
+"/mnt/data/bit/xbh/_visda2022/visda2022-ours/work_dirs_mj/local-exp1/220906_2122_zerov12zerov2_dacs_a999_fdthings_daformer_sepaspp_mitb5_poly10warm_s0_77205/220906_2122_zerov12zerov2_dacs_a999_fdthings_daformer_sepaspp_mitb5_poly10warm_s0_77205.json" \
+"/mnt/data/bit/xbh/_visda2022/visda2022-ours/work_dirs_mj/local-exp1/220906_2122_zerov12zerov2_dacs_a999_fdthings_daformer_sepaspp_mitb5_poly10warm_s0_77205/latest.pth" \
+--show-dir ./preds_mj/exp1/palette --opacity 1
+
+# STEP 3: Converting for submission
+
+# This step converts visual labels to train id labels to prepare for submission
+CUDA_VISIBLE_DEVICES=0 python tools.convert_visuals_to_labels.py ./preds_mj/exp1/palette ./preds_mj/exp1/uda
+
+# STEP 4: Packing and zipping
+# First copy the source-only directory to `./preds_mj/exp1/`
+# The SegFormer source-only folder can be found as "/mnt/data/bit/xbh/_visda2022/visda2022-ours/preds_mj/v1_to_v2_src_segf/"
+cp -r ./preds_mj/v1_to_v2_src_segf ./preds_mj/exp1/source-only
+# Alternatively, soft link can be used or copied
+#1
+## ln -s /mnt/data/bit/xbh/_visda2022/visda2022-ours/preds_mj/v1_to_v2_src_segf/ ./preds_mj/exp1/source-only
+#2
+## cp -d ./preds_mj/exp2/source-only ./preds_mj/exp1/
+
+# STEP 5: Zip and upload
+cd ./preds_mj/exp1/
+zip -q -r exp1_v1_to_v2.zip ./uda ./source-only
+
+# STEP 6: Submit
+# exp1_v1_to_v2.zip is all you need to submit
