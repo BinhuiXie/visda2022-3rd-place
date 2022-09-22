325ec2d83af6f780f33b82bfcecb14b659c727ce
BinhuiXie <binhuixie@bit.edu.cn>
2022-09-22 23:25:13 +0800

    neurips2022 self-training

--- mmseg/models/uda/__init__.py
+++ mmseg/models/uda/__init__.py
@@ -1,4 +1,6 @@
 from mmseg.models.uda.dacs import DACS
 from mmseg.models.uda.sepico import SePiCo
+from mmseg.models.uda.fst_d import NESTEROV
+from mmseg.models.uda.fst_w import Wider_NESTEROV_DifData
 
-__all__ = ['DACS', 'SePiCo']
+__all__ = ['DACS', 'SePiCo', 'NESTEROV', 'Wider_NESTEROV_DifData']
