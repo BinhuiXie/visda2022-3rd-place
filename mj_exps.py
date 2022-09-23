b93bea23ac2d2c45d2e888bf3d56b23bfa5fb8fa
KiwiXR <kiwi2364261522@outlook.com>
2022-09-23 19:02:03 +0800

    dwt mix

--- mj_exps.py
+++ mj_exps.py
@@ -148,50 +148,22 @@ def generate_experiment_cfgs(id):
             cfg['_base_'].append(
                 f'_base_/datasets/uda_{source}_to_{target}_{crop}.py')
             cfg['_base_'].append(f'_base_/uda/{uda}.py')
-
-        cfg.setdefault('uda', {})
-        if method_name in uda and plcrop:
+        if 'dacs' in uda and plcrop:
+            cfg.setdefault('uda', {})
             cfg['uda']['pseudo_weight_ignore_top'] = 15
             cfg['uda']['pseudo_weight_ignore_bottom'] = 120
         cfg['data'] = dict(
             samples_per_gpu=batch_size,
             workers_per_gpu=workers_per_gpu,
             train={})
-        if method_name in uda and rcs_T is not None:
+        if 'dacs' in uda and rcs_T is not None:
             cfg = setup_rcs(cfg, rcs_T)
 
-        if method_name in uda:
-            cfg['uda']['start_distribution_iter'] = start_distribution_iter
-            if use_bank:
-                cfg['uda']['memory_length'] = memory_length
-
-            cfg['model'].setdefault('decode_head', {})
-            cfg['model'].setdefault('auxiliary_head', {})
-            cfg['model']['auxiliary_head']['in_channels'] = in_channels
-            cfg['model']['auxiliary_head']['in_index'] = contrast_indexes
-            cfg['model']['auxiliary_head']['input_transform'] = contrast_mode
-            cfg['model']['auxiliary_head']['channels'] = channels
-            cfg['model']['auxiliary_head']['num_convs'] = num_convs
-            if num_convs == 0:
-                if contrast_mode == 'resize_concat':
-                    cfg['model']['auxiliary_head']['channels'] = sum(in_channels)
-                else:
-                    cfg['model']['auxiliary_head']['channels'] = in_channels
-            cfg['model']['auxiliary_head'].setdefault('loss_decode', {})
-            cfg['model']['auxiliary_head']['loss_decode']['use_dist'] = use_dist
-            cfg['model']['auxiliary_head']['loss_decode']['use_bank'] = use_bank
-            cfg['model']['auxiliary_head']['loss_decode']['use_reg'] = use_reg
-            cfg['model']['auxiliary_head']['loss_decode']['use_avg_pool'] = use_avg_pool
-            cfg['model']['auxiliary_head']['loss_decode']['scale_min_ratio'] = scale_min_ratio
-            cfg['model']['auxiliary_head']['loss_decode']['contrast_temp'] = contrastive_temperature
-            cfg['model']['auxiliary_head']['loss_decode']['loss_weight'] = contrastive_weight
-            cfg['model']['auxiliary_head']['loss_decode']['reg_relative_weight'] = reg_relative_weight
-
-        if method_name in uda and enable_self_training:
-            cfg['uda']['enable_self_training'] = enable_self_training
+        if 'dacs' in uda and dwt_alpha is not None:
+            cfg['uda']['dwt_alpha'] = dwt_alpha
 
         # Setup optimizer and schedule
-        if method_name in uda:
+        if 'dacs' in uda:
             cfg['optimizer_config'] = None  # Don't use outer optimizer
 
         cfg['_base_'].extend(
@@ -214,32 +186,12 @@ def generate_experiment_cfgs(id):
 
         # Construct config name
         uda_mod = uda
-
-        if method_name in uda:
-            if use_dist:
-                uda_mod += '_DistCL'
-            elif use_bank:
-                uda_mod += '_BankCL'
-            else:
-                uda_mod += '_ProtoCL'
-            if use_reg:
-                uda_mod += f'-reg-w{reg_relative_weight * contrastive_weight}'
-            uda_mod += f'-start-iter{start_distribution_iter}'
-            uda_mod += f'-tau{contrastive_temperature}'
-            if contrast_mode == 'multiple_select':
-                for lyr in contrast_indexes:
-                    uda_mod += f'-l{lyr}-w{contrastive_weight}'
-            else:
-                uda_mod += f'-l{contrast_indexes}-w{contrastive_weight}'
-
-        if method_name in uda and rcs_T is not None:
+        if 'dacs' in uda and rcs_T is not None:
             uda_mod += f'_rcs{rcs_T}'
-        if method_name in uda and plcrop:
+        if 'dacs' in uda and plcrop:
             uda_mod += '_cpl'
-        if method_name in uda and enable_self_training:
-            uda_mod += '_self'
-
-        # Construct config name
+        cfg['name'] = f'{source}2{target}_{uda_mod}_{architecture_mod}_' \
+                      f'{backbone}_{schedule}'
         cfg['exp'] = id
         cfg['name_dataset'] = f'{source}2{target}'
         cfg['name_architecture'] = f'{architecture_mod}_{backbone}'
@@ -248,10 +200,9 @@ def generate_experiment_cfgs(id):
         cfg['name_uda'] = uda_mod
         cfg['name_opt'] = f'{opt}_{lr}_pm{pmult}_{schedule}' \
                           f'_{n_gpus}x{batch_size}_{iters // 1000}k'
-        cfg['name'] = f"{cfg['name_architecture']}_{cfg['name_uda']}_{cfg['name_opt']}_{cfg['name_dataset']}"
         if seed is not None:
-            cfg['name'] += f'_seed{seed}'
-        cfg['name'] = cfg['name'].replace('.', '.').replace('True', 'T') \
+            cfg['name'] += f'_s{seed}'
+        cfg['name'] = cfg['name'].replace('.', '').replace('True', 'T') \
             .replace('False', 'F').replace('cityscapes', 'cs') \
             .replace('synthia', 'syn')
         return cfg
@@ -260,7 +211,6 @@ def generate_experiment_cfgs(id):
     # Set some defaults
     # -------------------------------------------------------------------------
     cfgs = []
-    method_name = 'sepico'
     n_gpus = 1
     batch_size = 2
     iters = 40000
@@ -269,30 +219,13 @@ def generate_experiment_cfgs(id):
     datasets = [
         ('zerowaste', 'zerowaste'),
     ]
-
-    rcs_T = None
-    plcrop = True
-    start_distribution_iter = 3000
-    enable_self_training = True
-
-    # auxiliary head parameters
-    in_channels = 2048  # in_channels = [256, 512, 1024, 2048]
-    channels = 512  # default out_dim
-    num_convs = 2
-    contrast_indexes = 3  # int or list, depending on value of contrast_mode
-    contrast_mode = None  # optional(None, 'resize_concat', 'multiple_select')
-    use_dist = False
-    use_bank = False
-    memory_length = 200
-    use_reg = False
-    use_avg_pool = True
-    scale_min_ratio = 0.75  # used for down-sampling
-    contrastive_temperature = 100.
-    contrastive_weight = 1.0
-    reg_relative_weight = 1.0  # reg_weight = reg_relative_weight * loss_weight in auxiliary head
-
     architecture = None
     workers_per_gpu = 4
+    rcs_T = None
+    plcrop = False
+
+    # dwt mix
+    dwt_alpha = None
     # -------------------------------------------------------------------------
     # VisDA official baseline: seed=0 official#v2val{50.84}; seed=0 repro#v2val{51.69}
     # -------------------------------------------------------------------------
@@ -424,184 +357,54 @@ def generate_experiment_cfgs(id):
             cfg = config_from_vars()
             cfgs.append(cfg)
     # -------------------------------------------------------------------------
-    # try sepico (DistCL)
+    # dwt mix dwt_alpha=0.5
     # -------------------------------------------------------------------------
-    elif id == 7:
+    elif id == 13:
         seeds = [0]
         datasets = [
             ('zerov1', 'zerov2'),
         ]
-        architecture, backbone = ('daformer_sepaspp_proj', 'mitb5')
-        # udas = ['sepico', 'sepico_fdthings_zerowaste']
-        udas = ['sepico_fdthings_zerowaste']  # fix backword twice bug
-        # aux
-        num_convs = 2
-        modes = [
-            # in_channels, contrast_indexes, contrast_mode
-            ([64, 128, 320, 512], [0, 1, 2, 3], 'resize_concat'),  # fusion
-        ]
-        # reg
-        use_reg = True
-        reg_relative_weight = 1.0
-        # contrastive variants
-        methods = [
-            # use_dist, use_bank
-            (True, False),  # DistCL
-        ]
-        # results
-        for seed, uda, mode, (use_dist, use_bank), (source, target) in itertools.product(seeds, udas, modes, methods, datasets):
-            in_channels, contrast_indexes, contrast_mode = mode
+        architecture, backbone = ('daformer_sepaspp', 'mitb5')
+        uda = 'dacs_a999_fdthings_dwt_zerowaste'
+        # rcs_T = 0.01
+        plcrop = True
+        dwt_alpha = 0.5
+        for (source, target), seed in \
+                itertools.product(datasets, seeds):
             cfg = config_from_vars()
             cfgs.append(cfg)
     # -------------------------------------------------------------------------
-    # try sepico (BankCL)
+    # dwt mix dwt_alpha='rand'
     # -------------------------------------------------------------------------
-    elif id == 8:
+    elif id == 14:
         seeds = [0]
         datasets = [
             ('zerov1', 'zerov2'),
         ]
-        architecture, backbone = ('daformer_sepaspp_proj', 'mitb5')
-        # udas = ['sepico', 'sepico_fdthings_zerowaste']
-        udas = ['sepico_fdthings_zerowaste']  # fix backword twice bug
-        # aux
-        num_convs = 2
-        modes = [
-            # in_channels, contrast_indexes, contrast_mode
-            ([64, 128, 320, 512], [0, 1, 2, 3], 'resize_concat'),  # fusion
-        ]
-        # reg
-        use_reg = True
-        reg_relative_weight = 1.0
-        # contrastive variants
-        methods = [
-            # use_dist, use_bank
-            (False, True),  # BankCL
-        ]
-        # results
-        for seed, uda, mode, (use_dist, use_bank), (source, target) in itertools.product(seeds, udas, modes, methods,
-                                                                                         datasets):
-            in_channels, contrast_indexes, contrast_mode = mode
+        architecture, backbone = ('daformer_sepaspp', 'mitb5')
+        uda = 'dacs_a999_fdthings_dwt_zerowaste'
+        # rcs_T = 0.01
+        plcrop = True
+        dwt_alpha = 'rand'
+        for (source, target), seed in \
+                itertools.product(datasets, seeds):
             cfg = config_from_vars()
             cfgs.append(cfg)
     # -------------------------------------------------------------------------
-    # try sepico (ProtoCL)
+    # dwt mix dwt_alpha='fade'
     # -------------------------------------------------------------------------
-    elif id == 9:
+    elif id == 15:
         seeds = [0]
         datasets = [
             ('zerov1', 'zerov2'),
         ]
-        architecture, backbone = ('daformer_sepaspp_proj', 'mitb5')
-        # udas = ['sepico', 'sepico_fdthings_zerowaste']
-        udas = ['sepico_fdthings_zerowaste']  # fix backword twice bug
-        # aux
-        num_convs = 2
-        modes = [
-            # in_channels, contrast_indexes, contrast_mode
-            ([64, 128, 320, 512], [0, 1, 2, 3], 'resize_concat'),  # fusion
-        ]
-        # reg
-        use_reg = True
-        reg_relative_weight = 1.0
-        # contrastive variants
-        methods = [
-            # use_dist, use_bank
-            (False, False),  # ProtoCL
-        ]
-        # results
-        for seed, uda, mode, (use_dist, use_bank), (source, target) in itertools.product(seeds, udas, modes, methods, datasets):
-            in_channels, contrast_indexes, contrast_mode = mode
-            cfg = config_from_vars()
-            cfgs.append(cfg)
-    # -------------------------------------------------------------------------
-    # try sepico (DistCL)  syn+z1 t0 z2
-    # -------------------------------------------------------------------------
-    elif id == 10:
-        seeds = [0]
-        datasets = [
-            ('synaugzerov1', 'zerov2'),
-        ]
-        architecture, backbone = ('daformer_sepaspp_proj', 'mitb5')
-        udas = ['sepico', 'sepico_fdthings_zerowaste']
-        # aux
-        num_convs = 2
-        modes = [
-            # in_channels, contrast_indexes, contrast_mode
-            ([64, 128, 320, 512], [0, 1, 2, 3], 'resize_concat'),  # fusion
-        ]
-        # reg
-        use_reg = True
-        reg_relative_weight = 1.0
-        # contrastive variants
-        methods = [
-            # use_dist, use_bank
-            (True, False),  # DistCL
-        ]
-        # results
-        for seed, uda, mode, (use_dist, use_bank), (source, target) in itertools.product(seeds, udas, modes, methods,
-                                                                                         datasets):
-            in_channels, contrast_indexes, contrast_mode = mode
-            cfg = config_from_vars()
-            cfgs.append(cfg)
-    # -------------------------------------------------------------------------
-    # try sepico (BankCL)
-    # -------------------------------------------------------------------------
-    elif id == 11:
-        seeds = [0]
-        datasets = [
-            ('synaugzerov1', 'zerov2'),
-        ]
-        architecture, backbone = ('daformer_sepaspp_proj', 'mitb5')
-        udas = ['sepico', 'sepico_fdthings_zerowaste']
-        # aux
-        num_convs = 2
-        modes = [
-            # in_channels, contrast_indexes, contrast_mode
-            ([64, 128, 320, 512], [0, 1, 2, 3], 'resize_concat'),  # fusion
-        ]
-        # reg
-        use_reg = True
-        reg_relative_weight = 1.0
-        # contrastive variants
-        methods = [
-            # use_dist, use_bank
-            (False, True),  # BankCL
-        ]
-        # results
-        for seed, uda, mode, (use_dist, use_bank), (source, target) in itertools.product(seeds, udas, modes, methods,
-                                                                                         datasets):
-            in_channels, contrast_indexes, contrast_mode = mode
-            cfg = config_from_vars()
-            cfgs.append(cfg)
-    # -------------------------------------------------------------------------
-    # try sepico (ProtoCL)
-    # -------------------------------------------------------------------------
-    elif id == 12:
-        seeds = [0]
-        datasets = [
-            ('synaugzerov1', 'zerov2'),
-        ]
-        architecture, backbone = ('daformer_sepaspp_proj', 'mitb5')
-        udas = ['sepico', 'sepico_fdthings_zerowaste']
-        # aux
-        num_convs = 2
-        modes = [
-            # in_channels, contrast_indexes, contrast_mode
-            ([64, 128, 320, 512], [0, 1, 2, 3], 'resize_concat'),  # fusion
-        ]
-        # reg
-        use_reg = True
-        reg_relative_weight = 1.0
-        # contrastive variants
-        methods = [
-            # use_dist, use_bank
-            (False, False),  # ProtoCL
-        ]
-        # results
-        for seed, uda, mode, (use_dist, use_bank), (source, target) in itertools.product(seeds, udas, modes, methods,
-                                                                                         datasets):
-            in_channels, contrast_indexes, contrast_mode = mode
+        architecture, backbone = ('daformer_sepaspp', 'mitb5')
+        uda = 'dacs_a999_fdthings_dwt_zerowaste'
+        # rcs_T = 0.01
+        plcrop = True
+        dwt_alpha = 'fade'
+        for (source, target), seed in \
+                itertools.product(datasets, seeds):
             cfg = config_from_vars()
             cfgs.append(cfg)
     else:
