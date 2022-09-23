6384976f7f1943ae0990f17b7dc874d93f02b5c6
KiwiXR <kiwi2364261522@outlook.com>
2022-09-23 17:48:04 +0800

    dwt mix

--- mmseg/models/utils/wavelet.py
+++ mmseg/models/utils/wavelet.py
@@ -7,13 +7,13 @@ ifm = IDWT(mode='zero', wave='haar').cuda()
 
 
 def dwt_copy_paste(mask, copy_img, paste_img, alpha=0.5):
+    with torch.no_grad():
+        Yl_copy, Yh_copy = xfm(copy_img).detach()
+        Yl_paste, Yh_paste = xfm(paste_img).detach()
 
-    Yl_copy, Yh_copy = xfm(copy_img)
-    Yl_paste, Yh_paste = xfm(paste_img)
+        Yl_mix = Yl_paste.clone()
+        Yl_mix[mask] = Yl_copy[mask] * alpha + Yl_paste[mask] * (1 - alpha)
+        Yh_mix = [torch.maximum(a, b) for (a, b) in zip(Yh_copy, Yh_paste)]
 
-    Yl_mix = Yl_paste.clone()
-    Yl_mix[mask] = Yl_copy[mask] * alpha + Yl_paste[mask] * (1 - alpha)
-    Yh_mix = [torch.maximum(a, b) for (a, b) in zip(Yh_copy, Yh_paste)]
-
-    mix_img = ifm((Yl_mix, Yh_mix))
+        mix_img = ifm((Yl_mix, Yh_mix)).detach()
     return mix_img
