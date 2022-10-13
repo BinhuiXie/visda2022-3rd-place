e7a40ab47fdd8880fb4c2683041efffddf6865d9
binhuixie <binhuixie@bit.edu.cn>
2022-10-13 14:51:35 +0800

    update

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -1,12 +1,11 @@
 from .accuracy import Accuracy, accuracy
 from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy, mask_cross_entropy)
-from .logit_constraint_loss import LogitConstraintLoss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 from .contrastive_loss import ContrastiveLoss
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
-    'mask_cross_entropy', 'CrossEntropyLoss', 'LogitConstraintLoss',
+    'mask_cross_entropy', 'CrossEntropyLoss',
     'reduce_loss', 'weight_reduce_loss', 'weighted_loss', 'ContrastiveLoss'
 ]
--- mmseg/models/uda/__init__.py
+++ mmseg/models/uda/__init__.py
@@ -1,7 +1,4 @@
 from mmseg.models.uda.dacs import DACS
 from mmseg.models.uda.sepico import SePiCo
-from mmseg.models.uda.fst_d import NESTEROV
-from mmseg.models.uda.fst_w import Wider_NESTEROV_DifData
-from mmseg.models.uda.dacs_dwt_mix import DACSDWTMix
 
-__all__ = ['DACS', 'SePiCo', 'NESTEROV', 'Wider_NESTEROV_DifData', 'DACSDWTMix']
+__all__ = ['DACS', 'SePiCo']
